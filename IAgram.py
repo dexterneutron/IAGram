b38a55f726a69370d08d04c50a98e22348ae553e
Felix David Lugo Guevara <FelixDavid.LugoGuevara@halliburton.com>
2020-08-25 15:19:14 -0400

    Added IAgram Filter Module

---
+++ IAgram.py
@@ -0,0 +1,64 @@
+from PIL import Image, ImageDraw,ImageFont,ImageFilter
+
+def IATransform(filter,sentence,i):
+    if filter=='WhiteText':
+        fnt = ImageFont.truetype('CroissantOne-Regular.ttf', 50)
+        brightness=0.3
+        img=BrightnessFilter(i, brightness)
+        img=AddcenteredText(sentence,img,fnt)
+        return img
+    else:
+        print('Filter doesnt exist')
+
+def AddcenteredText(sentence,img,fnt):
+    d = ImageDraw.Draw(img)
+    x1 = 612
+    y1 = 612
+    sum=0
+    for letter in sentence:
+        sum += d.textsize(letter, font=fnt)[0]
+        average_length_of_letter = sum/len(sentence)
+        #find the number of letters to be put on each line
+        number_of_letters_for_each_line = (x1/1.618)/average_length_of_letter
+        incrementer = 0
+        fresh_sentence = ''
+    #add some line breaks
+    for letter in sentence:
+        if(letter == '-'):
+            fresh_sentence += '\n\n' + letter
+        elif(incrementer < number_of_letters_for_each_line):
+            fresh_sentence += letter
+        else:
+            if(letter == ' '):
+                fresh_sentence += '\n'
+                incrementer = 0
+            else:
+                fresh_sentence += letter
+        incrementer+=1
+    print (fresh_sentence)
+    #render the text in the center of the box
+    dim = d.textsize(fresh_sentence, font=fnt)
+    x2 = dim[0]
+    y2 = dim[1]
+    qx = (x1/2 - x2/2)
+    qy = (y1/2-y2/2)
+    #d.text((qx-1, qy), fresh_sentence, align="center",font=fnt, fill=(0,0,0))
+    #d.text((qx+1, qy), fresh_sentence,align="center", font=fnt, fill=(0,0,0))
+    #d.text((qx, qy-1), fresh_sentence,align="center", font=fnt, fill=(0,0,0))
+    #d.text((qx, qy+1), fresh_sentence,align="center", font=fnt, fill=(0,0,0))
+    d.text((qx,qy), fresh_sentence ,align="center",  font=fnt, fill=(255,255,255))
+    return img
+
+def BrightnessFilter(i, br):
+    img = Image.new('RGB', i.size)
+    for x in range(i.size[0]):
+        for y in range(i.size[1]):
+            r, g, b = i.getpixel((x, y))
+            red = int(r * br)
+            red = min(255, max(0, red))
+            green = int(g * br)
+            green = min(255, max(0, green))
+            blue = int(b * br)
+            blue = min(255, max(0, blue))
+            img.putpixel((x, y), (red, green, blue))
+    return img
---
+++ V0.2/Beet2.py
@@ -0,0 +1,68 @@
+from PIL import Image, ImageDraw,ImageFont,ImageFilter
+#variables for image size
+x1 = 612
+y1 = 612
+size=612,612
+#my quote
+sentence = "Some people want it to happen, some wish it would happen, others make it happen. -Michael Jordan"
+fnt = ImageFont.truetype('CroissantOne-Regular.ttf', 50)
+
+#img = Image.new('RGB', (x1, y1), color = (255, 255, 255))
+i=Image.open("background3.jpg")
+i=i.resize(size)
+#img = img.filter(ImageFilter.BoxBlur(2))
+#img.putalpha(128)
+brightness=0.3
+img = Image.new('RGB', i.size)
+for x in range(i.size[0]):
+    for y in range(i.size[1]):
+      r, g, b = i.getpixel((x, y))
+
+      red = int(r * brightness)
+      red = min(255, max(0, red))
+
+      green = int(g * brightness)
+      green = min(255, max(0, green))
+
+      blue = int(b * brightness)
+      blue = min(255, max(0, blue))
+
+      img.putpixel((x, y), (red, green, blue))
+
+d = ImageDraw.Draw(img)
+#find the average size of the letter
+sum = 0
+for letter in sentence:
+  sum += d.textsize(letter, font=fnt)[0]
+average_length_of_letter = sum/len(sentence)
+#find the number of letters to be put on each line
+number_of_letters_for_each_line = (x1/1.618)/average_length_of_letter
+incrementer = 0
+fresh_sentence = ''
+#add some line breaks
+for letter in sentence:
+  if(letter == '-'):
+    fresh_sentence += '\n\n' + letter
+  elif(incrementer < number_of_letters_for_each_line):
+    fresh_sentence += letter
+  else:
+    if(letter == ' '):
+      fresh_sentence += '\n'
+      incrementer = 0
+    else:
+      fresh_sentence += letter
+  incrementer+=1
+print (fresh_sentence)
+#render the text in the center of the box
+dim = d.textsize(fresh_sentence, font=fnt)
+x2 = dim[0]
+y2 = dim[1]
+qx = (x1/2 - x2/2)
+qy = (y1/2-y2/2)
+#d.text((qx-1, qy), fresh_sentence, align="center",font=fnt, fill=(0,0,0))
+#d.text((qx+1, qy), fresh_sentence,align="center", font=fnt, fill=(0,0,0))
+#d.text((qx, qy-1), fresh_sentence,align="center", font=fnt, fill=(0,0,0))
+#d.text((qx, qy+1), fresh_sentence,align="center", font=fnt, fill=(0,0,0))
+d.text((qx,qy), fresh_sentence ,align="center",  font=fnt, fill=(255,255,255))
+
+img.save('quote3.png')
